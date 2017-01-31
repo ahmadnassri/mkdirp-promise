a43f7ac04b721d4ee7ff8885d2e7ad6e6c18e748
Ahmad Nassri <ahmad@ahmadnassri.com>
2017-01-31 14:15:51 -0500

    chore(dependency): update node-tap to v10 & fix tests

--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ tap.beforeEach(done => mkdir(base, done))
 tap.afterEach(done => rimraf(base, done))
 
 tap.test('should successfully create a directory tree', assert => {
-  tap.plan(3)
+  assert.plan(1)
 
   let x = Math.floor(Math.random() * Math.pow(16, 4)).toString(16)
   let y = Math.floor(Math.random() * Math.pow(16, 4)).toString(16)
@@ -20,13 +20,13 @@ tap.test('should successfully create a directory tree', assert => {
 
   let file = path.join(base, path.join.apply(null, [x, y, z]))
 
-  return mkdirp(file).then((made) => assert.equal(made, path.resolve(path.join(base, x))))
+  return mkdirp(file).then(made => assert.equal(made, path.resolve(path.join(base, x))))
 })
 
 tap.test('should catch thrown errors', assert => {
   assert.plan(1)
 
-  return mkdirp(true).catch((err) => assert.match(err, /^TypeError/))
+  return mkdirp(true).catch(err => assert.match(err, /^TypeError/))
 })
 
 tap.test('should catch errors', assert => {
